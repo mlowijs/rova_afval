4fbac68c55d4cdfe553f9b31080427a2c922c3f9
Michiel Lowijs <michiel.lowijs@sycade.com>
2018-04-01 12:35:09 +0200

    new version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='rova_afval',
     packages=['rova_afval'],
-    version='0.1',
+    version='0.2',
     description='ROVA Afvalkalender API',
     author='M. Lowijs',
     author_email='mlowijs@gmail.com',
     url='https://github.com/mlowijs/rova_afval',
-    download_url='https://github.com/mlowijs/rova_afval/archive/0.1.tar.gz',
+    download_url='https://github.com/mlowijs/rova_afval/archive/0.2.tar.gz',
     keywords=['rova', 'afval'],
     classifiers=[]
 )
