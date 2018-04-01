b8f8aa531fbbbc8b950bc41e4cf4199fdc90bb2d
Michiel Lowijs <michiel.lowijs@sycade.com>
2018-04-01 12:54:59 +0200

    small fixes

--- rova_afval/__init__.py
+++ rova_afval/__init__.py
@@ -3,15 +3,19 @@ from datetime import date
 
 ROVA_API_URL = 'http://rova.quintor.nl/rest/afvalkalender/{0}/{1}/{2}/{3}?toevoeging={4}'
 
-def get_inzamelingen(year, postcode, house_number, suffix = ""):
+def get_inzamelingen(year, postcode, house_number, suffix = None):
 
-    postcode = postcode.strip()
+    postcode = str(postcode).strip()
     postcode_letters = postcode[0:4]
     postcode_numbers = postcode[4:].strip()
 
-    year = year.strip()
-    house_number = house_number.strip()
-    suffix = suffix.strip()
+    year = str(year).strip()
+    house_number = str(house_number).strip()
+
+    if suffix == None:
+        suffix = ""
+    
+    suffix = str(suffix).strip()
 
     request_url = ROVA_API_URL.format(year, postcode_letters, postcode_numbers, house_number, suffix)
     response = requests.get(request_url).json()
